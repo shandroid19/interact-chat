e04fe66c9d388f2f3beb4e53a4cdd3fbaa0c9c16
shandroid19 <shanthanucn@gmail.com>
2021-08-05 09:41:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const PORT = process.env.PORT || 8900;
+const io = require('socket.io')(PORT,{
+    cors:{
+        origin:'http://localhost:3000',
+    },
+})
+var users=[]
+
+const addUser = (userId, socketId)=>{
+    !users.some((user)=>user.userId===userId) &&  
+    users.push({userId,socketId})
+}
+
+const removeUser = (socketId)=>{
+    users = users.filter((user)=>user.socketId!== socketId)
+}
+
+const getUser=(userId)=>{
+    return users.find(user=>user.userId === userId)
+}
+
+
+io.on("connection",(socket)=>{
+
+    io.emit("a user connected")
+
+    socket.on('addUser',userId=>{
+        console.log(userId,socket.id)
+        addUser(userId,socket.id)
+        io.emit('getUsers',users)
+    })
+    
+    socket.on('sendMessage',({sender,receiverId,message})=>{
+        console.log(receiverId,users)
+        const user = getUser(receiverId);
+        if(user){
+        io.to(user.socketId).emit("getMessage",{
+            sender,message,
+        })
+    }
+    })
+
+    socket.on("disconnect",()=>{
+        removeUser(socket.id);
+        io.emit('getUsers',users)
+    })
+    })
